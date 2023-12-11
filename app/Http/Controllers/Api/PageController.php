27a8ee3592d4da94f5d4d67f59f3cc0f63b65dc8
alessandrodaniello105 <kanymsevement@gmail.com>
2023-12-11 13:18:40 +0100

    [add]: image absolute url in getProject api

--- app/Http/Controllers/Api/PageController.php
+++ app/Http/Controllers/Api/PageController.php
@@ -18,9 +18,16 @@ class PageController extends Controller
 
     public function getProject($slug) {
 
+        // seleziono il progetto che ha lo slug uguale a quello passato, poi aggancio le tabelle in relazione con 'with'
+        // inoltre aggiungo un flag 'success' per sapere, lato front, se quello slug è stato trovato o meno
         $project = Project::where('slug', $slug)->with('type', 'technologies')->first();
         if($project) $success = true;
         else $success = false;
+
+        // come altro dato all'api passo il link assoluto dell'immagine nello storage, se c'è
+        // se non c'è passo il link dell'immagine placeholder
+        if($project->image) $project->image = asset('storage/' . $project->image);
+        else $project->image = asset('img/placeholder.webp');
 
         return response()->json(compact('project', 'success'));
 
